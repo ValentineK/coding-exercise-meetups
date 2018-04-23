5342ee49197af9e6eeaac998be178bfcb8e95c2f
Valentin Konov <vk@12auto.de>
2018-04-23 11:52:38 +0200

    add .env files fix small bug in title

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,7 +2,7 @@ module GroupsHelper
 
   def page_title
     # Its a bit ugly here, I usualliy use gem 'meta-tags', but since it is only two pages...
-    ['Meetup client', @search_text, @search_text.nil?? '' : 'Search results'].join(' - ')
+    ['Meetup client', @search_text, @search_text.nil?? nil : 'Search results'].compact.join(' - ')
   end
 
 end
